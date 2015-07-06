20bdb702a801822ce01a9795014dbd06e67427a9
Alexei Barantsev <barancev@gmail.com>
2015-07-06 19:43:30 +0300

    Добавлены все остальные тесты для поиска

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,6 +2,18 @@
 import pytest
 
 
+@pytest.mark.parametrize("query", [
+    ("Neumann"),
+    ("кинокамера"),
+    ("объектив canon"),
+])
+def test_search_positiff(app, query):
+    app.open_home_page()
+    app.search.find(query)
+    assert app.search.get_result_count() > 0
+    assert query in app.search.get_all_results_text()
+
+
 @pytest.mark.parametrize("query,expected", [
     ("", "Вы задали пустой поисковый запрос"),
     ("a", "Слишком короткий запрос"),
@@ -9,6 +21,16 @@ import pytest
     ("sony", "Найдено"),
 ])
 def test_search_by_query_string_of_various_length(app, query, expected):
+    app.open_home_page()
+    app.search.find(query)
+    assert app.search.get_result_message().startswith(expected)
+
+
+@pytest.mark.parametrize("query,expected", [
+    ("千代に 細石", "Некорректный запрос. Попробуйте изменить запрос"),
+    ("камера 千代に Arri", "Поиск осуществлен по ключевым словам « камера Arri », по « 千代に » поиск не возможен."),
+])
+def test_search_in_wrong_encoding(app, query, expected):
     app.open_home_page()
     app.search.find(query)
     assert app.search.get_result_message().startswith(expected)
@@ -28,3 +50,21 @@ def test_search_by_patterns(app, query, expected):
     app.search.find(query)
     assert app.search.get_result_count() > 0
     assert expected in app.search.get_all_results_text()
+
+
+@pytest.mark.parametrize("query1,query2", [
+    ("оборудование", "ОБОРУДОВАНИЕ"),
+    ("оборудование", "Оборудование"),
+    ("оборудование", "обоРудование"),
+    ("оборудование", "ОбОРУдОвАнИЕ"),
+    ("видео монтаж", "монтаж видео"),
+    ("фестиваль", "фёстиваль"),
+])
+def test_search_is_case_insensitive_and_order_independent_and_yo(app, query1, query2):
+    app.open_home_page()
+    app.search.find(query1)
+    res1 = app.search.get_result_count()
+    app.search.find(query2)
+    res2 = app.search.get_result_count()
+    assert res1 > 0
+    assert res1 == res2
