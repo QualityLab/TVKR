07f0bc9b5c61458fb1a6b5b9dfeaf9bd2a04c4e2
Alexei Barantsev <barancev@gmail.com>
2015-07-06 16:54:29 +0300

    Сделана заготовка инфраструктуры под использование py.test

---
+++ conftest.py
@@ -0,0 +1,49 @@
+__author__ = 'alexei'
+
+import pytest
+import json
+import os.path
+import ftputil
+from fixture.application import Application
+
+fixture = None
+target = None
+
+
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
+
+
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+
+@pytest.fixture
+def app(request, config):
+    global fixture
+    browser = request.config.getoption("--browser")
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, config=config)
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        if fixture in not None:
+            fixture.session.ensure_logout()
+            fixture.destroy()
+    request.addfinalizer(fin)
+    return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--target", action="store", default="target.json")
+
---
+++ fixture/application.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+
+
+class Application:
+
+    def __init__(self, browser, config):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.config = config
+        self.base_url = config['web']['baseUrl']
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get(self.base_url)
+
+    def destroy(self):
+        self.wd.quit()
+
