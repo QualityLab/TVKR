adc346fc3495e004da368eab3e283c66917a9ab8
Alexei Barantsev <barancev@gmail.com>
2015-08-10 17:15:32 +0300

    Добавлено ожидание исчезновения старой ссылки с таким же текстом, устранено исключение StaleElementReferenceException

--- tests/company_search_strong_match.py
+++ tests/company_search_strong_match.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-from baseurl import Baseurl
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from .baseurl import Baseurl
 import time, unittest
 
 def is_alert_present(wd):
@@ -14,16 +15,18 @@ def is_alert_present(wd):
 class company_search_strong_match(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-    
-    def test_(self):
+
+    def test_strong(self):
         success = True
         wd = self.wd
         wd.get(str(Baseurl.baseurl) + "companies")
+        company_links = wd.find_elements_by_link_text("3D Media Group")
         wd.find_element_by_id("keywords").click()
         wd.find_element_by_id("keywords").clear()
         wd.find_element_by_id("keywords").send_keys("3D Media Group")
         wd.find_element_by_id("filter-by-keywords").click()
+        if len(company_links) > 0:
+            WebDriverWait(wd, 30).until(EC.staleness_of(company_links[0]))
         wd.find_element_by_link_text("3D Media Group").click()
         self.assertTrue(success)
     
