80dd177e1420923df317e83fbc3bff58dad4c55d
Alexei Barantsev <barancev@gmail.com>
2015-08-10 17:30:29 +0300

    Тесты переделаны под py.test; добавлены явные ожидания (а неявные отключены)

--- tests/companies_search_partly_match.py
+++ tests/companies_search_partly_match.py
@@ -1,34 +1,16 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
-from baseurl import Baseurl
+from selenium.webdriver.support.wait import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
-
-class companies_search_partly_match(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-    
-    def test_companies_search_partly_match(self):
-        success = True
-        wd = self.wd
-        wd.get(str(Baseurl.baseurl) + "companies")
-        wd.find_element_by_id("keywords").click()
-        wd.find_element_by_id("keywords").clear()
-        wd.find_element_by_id("keywords").send_keys("русские студии")
-        wd.find_element_by_id("filter-by-keywords").click()
-        wd.find_element_by_link_text("Всемирные Русские Студии - Санкт-Петербург").click()
-        self.assertTrue(success)
-    
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_companies_search_partly_match(app):
+    wd = app.wd
+    wd.get("%s/%s" % (app.base_url, "companies"))
+    company_links = wd.find_elements_by_link_text("3D Media Group")
+    wd.find_element_by_id("keywords").click()
+    wd.find_element_by_id("keywords").clear()
+    wd.find_element_by_id("keywords").send_keys("русские студии")
+    wd.find_element_by_id("filter-by-keywords").click()
+    if len(company_links) > 0:
+        wait(wd, 30).until(EC.staleness_of(company_links[0]))
+    wait(wd, 30).until(lambda s: wd.find_element_by_link_text("Всемирные Русские Студии - Санкт-Петербург")).click()
+    # тут надо добавить ожидание загрузки страницы компании
--- tests/company_search_strong_match.py
+++ tests/company_search_strong_match.py
@@ -1,37 +1,16 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
-from .baseurl import Baseurl
-import time, unittest
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
-
-class company_search_strong_match(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-
-    def test_strong(self):
-        success = True
-        wd = self.wd
-        wd.get(str(Baseurl.baseurl) + "companies")
-        company_links = wd.find_elements_by_link_text("3D Media Group")
-        wd.find_element_by_id("keywords").click()
-        wd.find_element_by_id("keywords").clear()
-        wd.find_element_by_id("keywords").send_keys("3D Media Group")
-        wd.find_element_by_id("filter-by-keywords").click()
-        if len(company_links) > 0:
-            WebDriverWait(wd, 30).until(EC.staleness_of(company_links[0]))
-        wd.find_element_by_link_text("3D Media Group").click()
-        self.assertTrue(success)
-    
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_search_strong_match(app):
+    wd = app.wd
+    wd.get("%s/%s" % (app.base_url, "companies"))
+    company_links = wd.find_elements_by_link_text("3D Media Group")
+    wd.find_element_by_id("keywords").click()
+    wd.find_element_by_id("keywords").clear()
+    wd.find_element_by_id("keywords").send_keys("3D Media Group")
+    wd.find_element_by_id("filter-by-keywords").click()
+    if len(company_links) > 0:
+        wait(wd, 30).until(EC.staleness_of(company_links[0]))
+    wait(wd, 30).until(lambda s: wd.find_element_by_link_text("3D Media Group")).click()
+    # тут надо добавить ожидание загрузки страницы компании
