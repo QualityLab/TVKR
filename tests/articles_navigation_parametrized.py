caf09423b37f09edb38269605fb081db24673620
Alexei Barantsev <barancev@gmail.com>
2015-08-22 20:38:18 +0300

    Исправлен тест, который вроде бы начали переделывать на py.test, но не завершили

--- tests/articles_navigation_parametrized.py
+++ tests/articles_navigation_parametrized.py
@@ -1,31 +1,14 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
-from baseurl import Baseurl
+import pytest
+from selenium.webdriver.support.wait import WebDriverWait as wait
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
-class articles_navigation(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(30)
-	@pytest.mark.parametrize("locator,url", [
-	    ("//ul[@id='yw0']//a[.='Обзоры']", "article/category2"),
-	    ("//ul[@id='yw0']//a[.='Мнение']", "article/category7"),
-    ])
-	def test_articles_navigation(self, locator, url):
-        success = True
-        wd = self.wd
-        wd.get(str(Baseurl.baseurl) + "/article")
-		wd.find_element_by_xpath(str(locator)).click()
-		assert getCurrentUrl() == str(Baseurl.baseurl) + url
-    	self.assertTrue(success)
-	def tearDown(self):
-        self.wd.quit()
-if __name__ == '__main__':
-    unittest.main()		
+
+@pytest.mark.parametrize("link_text,url", [
+    ("Обзоры", "article/category2"),
+    ("Мнение", "article/category7"),
+])
+def test_articles_navigation(app, link_text, url):
+    wd = app.wd
+    app.open_page("article")
+    wd.find_element_by_xpath("//ul[@id='yw0']//a[.='%s']" % link_text).click()
+    wait(wd, 10).until(lambda s: wd.current_url == app.base_url + url)
