805e4f52e6680bc9cd3a281c354391e7d8e8b428
Alexei Barantsev <barancev@gmail.com>
2015-08-23 19:55:28 +0300

    Тест для создания вакансий доведен до работающего состояния

--- tests/check_vacancy_email.py
+++ tests/check_vacancy_email.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
+
+from selenium.webdriver.common.by import By
+from fixture.user import User
 from selenium.webdriver.support.wait import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.action_chains import ActionChains
-import time
+
 
 def test_check_vacancy_email(app):
     wd = app.wd
     actions = ActionChains(wd)
+    app.session.login_as(User(username="123@guerrillamail.com", password="1111", real_name="Сергей Петруненко"))
     app.open_page("resume") # размещаем вакансию с известным нам e-mail для ответа
     wd.find_element_by_xpath("//span[.='Разместить вакансию']").click()
-    app.login(username="123@guerrillamail.com", password="1111")
     wait(wd, 10).until(lambda s: wd.find_element_by_xpath("//input[@id='Vacancy_name']"))
     wd.find_element_by_xpath("//input[@id='Vacancy_name']").send_keys("Тест создания вакансии")
     wd.find_element_by_xpath("//span[@class='select2-chosen' and .='Проф. область']").click()
@@ -23,19 +27,13 @@ def test_check_vacancy_email(app):
     button = wd.find_element_by_xpath("//input[@class='btn btn_green btn_green_big']")
     actions.move_to_element(button).perform()
     wd.find_element_by_xpath("//input[@class='btn btn_green btn_green_big']").click()
-    time.sleep(3) # даем возможность отправиться данным на сервер, без этого ожидания фэйлится
-    app.open_page("logout")
-    app.open_page("login") # откликаемся на вакансию пользователем, у которого уже есть тестовое резюме 
-    app.login(username="z947384@yandex.ru", password="111111")
+    wait(wd, 10).until(lambda s: wd.find_element_by_css_selector("div.vacancy"))
+    app.session.login_as(User(username="z947384@yandex.ru", password="111111", real_name="Георгий Туманян"))
     app.open_page("job")
     wd.find_element_by_xpath("//a[.='Тест создания вакансии']").click()
-    wait(wd, 10).until(lambda s: wd.find_element_by_xpath("//span[@class='btn__inner']"))
-    wd.find_element_by_xpath("//span[@class='btn__inner']").click()
-    wd.find_element_by_xpath("//span[.='Отправить отклик']").click() #пробовал обратиться через разные атрибуты, в xpath элемент виден без проблем
-    time.sleep(5)
-    app.open_page("logout")
-    app.open_page("login") # проверяем на какой e-mail ушел отклик
-    app.login(username="testtvkinoradio@gmail.com", password="test12")
-    time.sleep(5) # даем время залогиниться
+    wait(wd, 10).until(lambda s: wd.find_element_by_css_selector("a.create-vacancy-response")).click()
+    wait(wd, 10).until(ec.visibility_of_element_located((By.CSS_SELECTOR, "div.vacancy-response-popup span.btn__inner"))).click()
+    wait(wd, 10).until(ec.visibility_of_element_located((By.CSS_SELECTOR, "div.response-sent")))
+    app.session.login_as(User(username="testtvkinoradio@gmail.com", password="test12", real_name="Test Test"))
     app.open_page("admin/emailQueue")
     assert "vacancy_testing@mail.com" == wd.find_element_by_xpath("//tbody/tr[1]/td[4]").text
